eaeb74640fdbcbbe2b2fb80ffe264f09525e0ba0
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-05-30 22:19:41 -0400

    Improve example plugin

---
+++ src/Aiws/Lexicon/Example/Plugin.php
@@ -0,0 +1,39 @@
+<?php namespace Aiws\Lexicon\Example;
+
+use Aiws\Lexicon\Contract\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    protected $attributes = array();
+
+    protected $content;
+
+    public $name;
+
+    public function getPluginName()
+    {
+        return $this->name;
+    }
+
+    public function setContent($content = '')
+    {
+        $this->content = $content;
+    }
+
+    public function setAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    public function attribute($name, $default = null, $offset = 0)
+    {
+        if (isset($this->attributes[$name])) {
+            return $this->attributes[$name];
+        } elseif (isset($this->attributes[$offset])) {
+            $this->attributes[$offset];
+            return $this->attributes[$offset];
+        }
+
+        return $default;
+    }
+}
--- src/Aiws/Lexicon/Example/TestPlugin.php
+++ src/Aiws/Lexicon/Example/TestPlugin.php
@@ -1,48 +1,24 @@
 <?php namespace Aiws\Lexicon\Example;
 
-use Aiws\Lexicon\Contract\PluginInterface;
-
-class TestPlugin implements PluginInterface
+class TestPlugin extends Plugin
 {
-    protected $attributes = array();
 
-    protected $content;
+    /**
+     * Plugin name
+     *
+     * @var string
+     */
+    public $name = 'test';
 
-    protected $dataTypes = [
-        'hello' => 'string'
-    ];
-
-    public function getPluginName()
-    {
-        return 'test';
-    }
-
+    /**
+     * Hello method
+     *
+     * @return string
+     */
     public function hello()
     {
         $name = $this->attribute('name', 'World');
 
-        return "Hello $name!";
-    }
-
-    public function setContent($content = '')
-    {
-        $this->content = $content;
-    }
-
-    public function setAttributes(array $attributes)
-    {
-        $this->attributes = $attributes;
-    }
-
-    public function attribute($name, $default = null, $offset = 0)
-    {
-        if (isset($this->attributes[$name])) {
-            return $this->attributes[$name];
-        } elseif (isset($this->attributes[$offset])) {
-            $this->attributes[$offset];
-            return $this->attributes[$offset];
-        }
-
-        return $default;
+        return "Hello {$name}!";
     }
 }
