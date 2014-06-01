c52ab4e432e422ba63d0baa7d48ce1bbacfdbf31
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-05-31 22:39:32 -0400

    Added Regex util

---
+++ src/Aiws/Lexicon/Util/Regex.php
@@ -0,0 +1,51 @@
+<?php namespace Aiws\Lexicon\Util;
+
+use Aiws\Lexicon\Contract\EnvironmentInterface;
+
+class Regex
+{
+    protected $lexicon;
+
+    public function __construct(EnvironmentInterface $lexicon)
+    {
+        $this->lexicon = $lexicon;
+    }
+
+    public function compress($string)
+    {
+        return preg_replace('/\s\s+/', ' ', $string);
+    }
+
+    /**
+     * Removes all of the comments from the text.
+     *
+     * @param  string $string Text to remove comments from
+     * @return string
+     */
+    public function parseComments($string)
+    {
+        return preg_replace('/\{\{#.*?#\}\}/s', '', $string);
+    }
+
+    public function extractNoParse($string)
+    {
+        preg_match_all('/\{\{\s*noparse\s*\}\}(.*?)\{\{\s*\/noparse\s*\}\}/ms', $string, $matches, PREG_SET_ORDER);
+
+        $extractions = array();
+
+        foreach ($matches as $match) {
+
+            $extraction = array(
+                'block'   => $match[0],
+                'hash'    => '__NO_PARSE__' . md5($match[0]),
+                'content' => $match[1],
+            );
+
+            $string        = str_replace($extraction['block'], $extraction['hash'], $string);
+            $extractions[] = $extraction;
+        }
+
+        return ['content' => $string, 'extractions' => $extractions];
+    }
+
+}
