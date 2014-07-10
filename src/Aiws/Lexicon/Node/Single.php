9c791de96fed7d164fd992aefb5172d0e30c66b6
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-07-09 23:13:52 -0500

    some named parameters for Single node

--- src/Aiws/Lexicon/Node/Single.php
+++ src/Aiws/Lexicon/Node/Single.php
@@ -9,10 +9,22 @@ abstract class Single extends Node
 
     public function getSetup(array $match)
     {
+        $name = isset($match[1]) ? $match[1] : null;
+
+        $name = isset($match['name']) ? $match['name'] : $name;
+
+        $parsedAttributes = isset($match[2]) ? $match[2] : null;
+
+        $parsedAttributes = isset($match['attributes']) ? $match['attributes'] : $parsedAttributes;
+
+        $extractionContent = isset($match[0]) ? $match[0] : null;
+
+        $extractionContent = isset($match['content']) ? $match['content'] : $extractionContent;
+
         $this
-            ->setName($match[1])
-            ->setParsedAttributes($match[2])
-            ->setExtractionContent($match[0]);
+            ->setName($name)
+            ->setParsedAttributes($parsedAttributes)
+            ->setExtractionContent($extractionContent);
     }
 
     public function getMatches($text)
