d414b1e4c8c326ae3b2fc8e046d2faf8d4b1b7ec
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-07-13 04:51:10 -0500

    doc blocks

--- src/Aiws/Lexicon/Lexicon.php
+++ src/Aiws/Lexicon/Lexicon.php
@@ -10,33 +10,96 @@ use Aiws\Lexicon\Util\Type;
 
 class Lexicon implements EnvironmentInterface
 {
+
+    /**
+     * Cache path
+     *
+     * @var
+     */
     public $cachePath;
 
+    /**
+     * Disable cache
+     *
+     * @var bool
+     */
     public $disableCache = true;
 
+    /**
+     * Compress source / remove excess white space
+     *
+     * @var bool
+     */
     public $compress = false;
 
+    /**
+     * Scope glue
+     *
+     * @var string
+     */
     public $scopeGlue = '.';
 
+    /**
+     * Max depth
+     *
+     * @var int
+     */
     public $maxDepth = 20;
 
+    /**
+     * Ignored matchers
+     *
+     * @var array
+     */
     public $ignoredMatchers = [];
 
+    /**
+     * No parse extractions
+     *
+     * @var array
+     */
     protected $noParseExtractions = array();
 
+    /**
+     * Plugin handler
+     *
+     * @var Contract\PluginHandlerInterface
+     */
     protected $pluginHandler;
 
+    /**
+     * Conditional
+     *
+     * @var ConditionalHandler
+     */
     protected $conditionalHandler;
 
+    /**
+     * Environment variable source
+     *
+     * @var string
+     */
     protected $environmentVariable = "array_except(get_defined_vars(), array('__data', '__path'))";
 
     /**
+     * Root node type
+     *
      * @var NodeInterface
      */
     public $rootNodeType;
 
+    /**
+     * Node types
+     *
+     * @var array
+     */
     public $nodeTypes = array();
 
+    /**
+     * Plugins
+     *
+     * @var array
+     */
     protected $plugins = array();
 
     /**
@@ -45,7 +108,6 @@ class Lexicon implements EnvironmentInterface
      * @var
      */
     protected $rootContextName = 'data';
-
 
     /**
      * Allow PHP
@@ -66,7 +128,13 @@ class Lexicon implements EnvironmentInterface
         $this->pluginHandler  = $pluginHandler;
     }
 
-    public function compile($content = null, $data = null)
+    /**
+     * Compile
+     *
+     * @param null $content
+     * @return string
+     */
+    public function compile($content = null)
     {
         if (!$content) {
             return null;
@@ -92,6 +160,12 @@ class Lexicon implements EnvironmentInterface
         return $this->compileRootNode($this->rootNodeType->make($setup));
     }
 
+    /**
+     * Compile root node
+     *
+     * @param NodeInterface $node
+     * @return mixed|string
+     */
     public function compileRootNode(NodeInterface $node)
     {
         $parsedNode = $node->createChildNodes();
@@ -115,21 +189,41 @@ class Lexicon implements EnvironmentInterface
         return $source;
     }
 
+    /**
+     * Get scope glue
+     *
+     * @return string
+     */
     public function getScopeGlue()
     {
         return $this->scopeGlue;
     }
 
+    /**
+     * Get node types
+     *
+     * @return array
+     */
     public function getNodeTypes()
     {
         return $this->nodeTypes;
     }
 
+    /**
+     * Get max depth
+     *
+     * @return int
+     */
     public function getMaxDepth()
     {
         return $this->maxDepth;
     }
 
+    /**
+     * Get plugin handler
+     *
+     * @return PluginHandlerInterface
+     */
     public function getPluginHandler()
     {
         return $this->pluginHandler->setEnvironment($this);
@@ -175,6 +269,12 @@ class Lexicon implements EnvironmentInterface
         return $text;
     }
 
+    /**
+     * Register root node type
+     *
+     * @param NodeInterface $nodeType
+     * @return EnvironmentInterface
+     */
     public function registerRootNodeType(NodeInterface $nodeType)
     {
         $nodeType->setEnvironment($this);
@@ -182,6 +282,12 @@ class Lexicon implements EnvironmentInterface
         return $this;
     }
 
+    /**
+     * Register node type
+     *
+     * @param NodeInterface $nodeType
+     * @return EnvironmentInterface
+     */
     public function registerNodeType(NodeInterface $nodeType)
     {
         $nodeType->setEnvironment($this);
@@ -189,6 +295,12 @@ class Lexicon implements EnvironmentInterface
         return $this;
     }
 
+    /**
+     * Register node types
+     *
+     * @param array $nodeTypes
+     * @return EnvironmentInterface
+     */
     public function registerNodeTypes(array $nodeTypes)
     {
         foreach($nodeTypes as $nodeType) {
@@ -197,54 +309,120 @@ class Lexicon implements EnvironmentInterface
         return $this;
     }
 
+    /**
+     * Register plugin
+     *
+     * @param $name
+     * @param $class
+     * @return EnvironmentInterface
+     */
     public function registerPlugin($name, $class)
     {
         $this->pluginHandler->register($name, $class);
         return $this;
     }
 
+    /**
+     * Get plugin
+     *
+     * @param $name
+     * @return Contract\PluginInterface
+     */
     public function getPlugin($name)
     {
         return $this->pluginHandler->get($name);
     }
 
+    /**
+     * Get variable
+     *
+     * @param             $data
+     * @param             $key
+     * @param array       $parameters
+     * @param string      $content
+     * @param null        $default
+     * @param null|string $expected
+     * @return mixed
+     */
     public function get($data, $key, $parameters = [], $content = '', $default = null, $expected = Type::ANY)
     {
         $context = new Context($this, $data);
         return $context->getVariable($key, $parameters, $content, $default, $expected);
     }
 
+    /**
+     * Compare in conditional expression
+     *
+     * @param      $left
+     * @param      $right
+     * @param null $operator
+     * @return bool
+     */
     public function compare($left, $right, $operator = null)
     {
         return $this->getConditionalHandler()->compare($left, $right, $operator);
     }
 
+    /**
+     * Call plugin method
+     *
+     * @param        $name
+     * @param array  $attributes
+     * @param string $content
+     * @return mixed
+     */
     public function call($name, $attributes = [], $content = '')
     {
         return $this->pluginHandler->call($name, $attributes, $content);
     }
 
+    /**
+     * Set ignored matchers
+     *
+     * @param array $ignoredMatchers
+     * @return EnvironmentInterface
+     */
     public function setIgnoredMatchers(array $ignoredMatchers = [])
     {
         $this->ignoredMatchers = $ignoredMatchers;
         return $this;
     }
 
+    /**
+     * Get ignored matchers
+     *
+     * @return string
+     */
     public function getIgnoredMatchers()
     {
         return implode('|', $this->ignoredMatchers);
     }
 
+    /**
+     * Get root node type
+     *
+     * @return NodeInterface
+     */
     public function getRootNodeType()
     {
         return $this->rootNodeType;
     }
 
+    /**
+     * Get regex
+     *
+     * @return Regex
+     */
     public function getRegex()
     {
         return $this->regex;
     }
 
+    /**
+     * Get root context name
+     *
+     * @return string
+     */
     public function getRootContextName()
     {
         return $this->rootContextName;
@@ -282,4 +460,5 @@ class Lexicon implements EnvironmentInterface
         $this->allowPhp = $allowPhp;
         return $this;
     }
+
 }
