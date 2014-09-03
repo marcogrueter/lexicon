c09d898ab9d27866a8bc3720112afbfaafe6cecc
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-09-03 09:38:58 -0500

    IgnoreVariable

--- src/Anomaly/Lexicon/Node/IgnoreVariable.php
+++ src/Anomaly/Lexicon/Node/IgnoreVariable.php
@@ -1,9 +1,36 @@
 <?php namespace Anomaly\Lexicon\Node;
 
-class IgnoreVariable
+class IgnoreVariable extends SingleRemain
 {
+
+    /**
+     * Do not compile as PHP
+     *
+     * @var bool
+     */
+    protected $isPhp = false;
+
+    /**
+     * Setup
+     *
+     * @param array $match
+     */
+    public function getSetup(array $match)
+    {
+        $this
+            ->setName(isset($match[2]) ? $match[2] : null)
+            ->setContent(isset($match[1]) ? $match[1] : null)
+            ->setExtractionContent(isset($match[0]) ? $match[0] : null);
+    }
+
+    /**
+     * Regex
+     *
+     * @return string
+     */
     public function regex()
     {
-        return '/^@(\{\{.*?\}\})/s';
+        return '/@(\{\{\s*(.*?)(\s.*?)?\s*(\/)?\}\})/ms';
     }
+
 }
