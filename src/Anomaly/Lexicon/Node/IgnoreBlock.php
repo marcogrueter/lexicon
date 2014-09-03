631cbdf3d54a610563b6a6e6f48b84d4475914d0
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-09-03 09:38:43 -0500

    IgnoreBlock

--- src/Anomaly/Lexicon/Node/IgnoreBlock.php
+++ src/Anomaly/Lexicon/Node/IgnoreBlock.php
@@ -1,9 +1,39 @@
 <?php namespace Anomaly\Lexicon\Node;
 
-class IgnoreBlock
+class IgnoreBlock extends Block
 {
+
+    /**
+     * Do not compile as PHP
+     *
+     * @var bool
+     */
+    protected $isPhp = false;
+
+    /**
+     * Defer compile
+     *
+     * @var bool
+     */
+    protected $deferCompile = true;
+
+    /**
+     * Get regex matcher
+     *
+     * @return string
+     */
     public function regex()
     {
-        return '/^@(\{\{.*?\}\})/s';
+        return '/\{\{\s*(ignore)(\s.*?)\}\}(.*?)\{\{\s*\/\1\s*\}\}/ms';
+    }
+
+    /**
+     * Compile content
+     *
+     * @return string
+     */
+    public function compile()
+    {
+        return $this->getContent();
     }
 }
