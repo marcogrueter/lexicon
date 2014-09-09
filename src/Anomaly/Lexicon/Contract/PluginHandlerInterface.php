e2c75b3302c4c011e1fcf6eea750f18b5f064052
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-09-09 01:03:53 -0500

    PluginInterface

--- src/Anomaly/Lexicon/Contract/PluginHandlerInterface.php
+++ src/Anomaly/Lexicon/Contract/PluginHandlerInterface.php
@@ -4,9 +4,46 @@ interface PluginHandlerInterface
 {
     public function setEnvironment(LexiconInterface $lexicon);
 
+    /**
+     * Register
+     *
+     * @param $name
+     * @param $class
+     * @return PluginHandlerInterface
+     */
     public function register($name, $class);
 
+    /**
+     * Get the plugin by name
+     *
+     * @param $name
+     * @return PluginInterface
+     */
     public function get($name);
 
+    /**
+     * Call the plugin method
+     *
+     * @param        $name
+     * @param array  $attributes
+     * @param string $content
+     * @return mixed
+     */
     public function call($name, $attributes = [], $content = '');
+
+    /**
+     * Plugin method is a filter
+     *
+     * @param $name
+     * @return bool
+     */
+    public function isFilter($name);
+
+    /**
+     * Plugin method is a parse-able filter
+     *
+     * @param $name
+     * @return bool
+     */
+    public function isParse($name);
 }
