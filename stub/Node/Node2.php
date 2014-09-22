dbd44a29df5d1ed7d1cc64a145ee3a03fd3f7b80
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-09-22 05:04:17 -0500

    stubs for phpspec

---
+++ stub/Node/Node2.php
@@ -0,0 +1,11 @@
+<?php namespace Anomaly\Lexicon\Stub\Node;
+
+/**
+ * Class Node2
+ *
+ * @package Anomaly\Lexicon\Stub\Node
+ */
+class Node2 extends Node
+{
+
+} 
---
+++ stub/Node/Node3.php
@@ -0,0 +1,11 @@
+<?php namespace Anomaly\Lexicon\Stub\Node;
+
+/**
+ * Class Node3
+ *
+ * @package Anomaly\Lexicon\Stub\Node
+ */
+class Node3 extends Node
+{
+
+} 
