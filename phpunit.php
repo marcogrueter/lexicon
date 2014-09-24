5fc060066cd1a8dd8974f83a85fdc9538d19bf8b
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-09-23 20:50:56 -0500

    remove phpunit

--- phpunit.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$vendorRoot = basename(dirname(dirname(__DIR__)));
-
-if ($vendorRoot == 'workbench') {
-    if (!@include_once __DIR__ . '/../../../vendor/autoload.php') {
-        exit("You must set up the project dependencies, run the following commands:\n> wget http://getcomposer.org/composer.phar\n> php composer.phar install\n");
-    }
-}
