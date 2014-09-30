7f53c45f2ad217fbd5b9c71f7c7e71ff9bc62d58
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-09-30 00:00:36 -0500

    Spec matchers

--- src/Anomaly/Lexicon/Test/Spec.php
+++ src/Anomaly/Lexicon/Test/Spec.php
@@ -1,5 +1,6 @@
 <?php namespace Anomaly\Lexicon\Test;
 
+use Anomaly\Lexicon\Contract\Node\NodeInterface;
 use PhpSpec\ObjectBehavior;
 
 /**
@@ -18,10 +19,35 @@ class Spec extends ObjectBehavior
     public function getMatchers()
     {
         return [
-            'haveValue' => function($subject, $value) {
+            'haveValue'     => function ($subject, $value) {
                 return in_array($subject, $value);
             },
+            'haveNodes'     => function ($nodeArray) {
+                return $this->hasNodes($nodeArray);
+            },
+            'haveNodeCount' => function ($nodeArray, $expectedCount = 0) {
+                return $this->hasNodes($nodeArray) and (count($nodeArray) == $expectedCount);
+            },
         ];
+    }
+
+    /**
+     * Has nodes
+     *
+     * @param array $nodeArray
+     * @return bool
+     */
+    public function hasNodes(array $nodeArray)
+    {
+        $hasNodes = true;
+        if (is_array($nodeArray)) {
+            foreach ($nodeArray as $node) {
+                if (!($node instanceof NodeInterface)) {
+                    $hasNodes = false;
+                }
+            }
+        }
+        return $hasNodes;
     }
 
 } 
