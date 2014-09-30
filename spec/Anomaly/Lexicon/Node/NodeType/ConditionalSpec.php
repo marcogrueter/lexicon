86fb0a6b23aa1fc4f7cdbbffe51f6fbe1c403bff
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-09-30 00:04:22 -0500

    conditional spec

--- spec/Anomaly/Lexicon/Node/NodeType/ConditionalSpec.php
+++ spec/Anomaly/Lexicon/Node/NodeType/ConditionalSpec.php
@@ -13,14 +13,24 @@ use Prophecy\Argument;
 class ConditionalSpec extends Spec
 {
 
-    function let(LexiconInterface $lexicon)
+    function let()
     {
-        $this->beConstructedWith($lexicon);
+        $this->beConstructedThrough('stub');
     }
 
     function it_is_initializable()
     {
         $this->shouldHaveType('Anomaly\Lexicon\Node\NodeType\Conditional');
+    }
+
+    function it_can_get_name_matcher()
+    {
+        $this->getNameMatcher()->shouldReturn('if|elseif|unless|elseunless');
+    }
+
+    function it_can_setup_regex_match()
+    {
+        $this->setup();
     }
 
     function it_can_get_construct_name()
@@ -29,6 +39,29 @@ class ConditionalSpec extends Spec
         $this->getConstructName()->shouldReturn('if');
         $this->setName('elseunless');
         $this->getConstructName()->shouldReturn('elseif');
+    }
+
+    function it_can_get_validator()
+    {
+        $this->getValidator()->shouldImplement('Anomaly\Lexicon\Contract\Node\ValidatorInterface');
+    }
+    
+    function it_can_create_child_nodes()
+    {
+        $this
+            ->setCurrentContent('foo or bar and baz && ying || yang')
+            ->createChildNodes()
+            ->getChildren()
+            ->shouldHaveNodeCount(9);
+    }
+    
+    function it_can_compile_source()
+    {
+        $this
+            ->setName('if')
+            ->setCurrentContent('foo or bar and baz && ying || yang')
+            ->compile()
+            ->shouldReturn('');
     }
 
 }
