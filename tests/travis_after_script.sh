8d470efc4c14b7645a290bdf4c9c2c79de5af2e1
Osvaldo Brignoni <obrignoni@aiwebsystems.com>
2014-11-02 05:35:10 -0600

    fix travis script

--- tests/travis_after_script.sh
+++ tests/travis_after_script.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_PHP_VERSION" != "hhvm" ]; then
     bin/test-reporter --stdout > codeclimate.json;
-    curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
-fi
+    curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports;
+fi;
